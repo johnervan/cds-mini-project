e2a00056ae71c9e591f50b675c5b4d3525e1dbf8
Johnervan Lee <johnervan.lee@accenture.com>
2020-03-30 13:02:07 +0800

    remove system out print

--- src/main/java/com/cdsminiproject/users/controller/api/UsersController.java
+++ src/main/java/com/cdsminiproject/users/controller/api/UsersController.java
@@ -1,6 +1,6 @@
 package com.cdsminiproject.users.controller.api;
 
-import com.cdsminiproject.users.controller.validator.NewUser;
+import com.cdsminiproject.users.controller.validator.NewUserValidator;
 import com.cdsminiproject.users.dto.model.UserDto;
 import com.cdsminiproject.users.dto.response.Response;
 import com.cdsminiproject.users.exception.CustomException;
@@ -28,24 +28,23 @@ public class UsersController {
 
 	@GetMapping("")
 	public Response getAllUsers() {
-		System.out.println(userService.findAll());
 		return Response.success().setData(userService.findAll());
 	}
 
 	@PostMapping("")
-	public Response createUser(@RequestBody @Valid NewUser newUser) {
-			return Response.success().setData(saveUser(newUser));
+	public Response createUser(@RequestBody @Valid NewUserValidator newUserValidator) {
+			return Response.success().setData(saveUser(newUserValidator));
 	}
 
 	@PostMapping("/upload")
 	public Response handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException, CustomException.InvalidUserCsvException {
-		List<NewUser> newUserList;
+		List<NewUserValidator> newUserList;
 		newUserList = fileToNewUserList(file);
 		return Response.success().setData(saveManyUsers(newUserList));
 	}
 
-	private List<NewUser> fileToNewUserList(MultipartFile file) throws IOException, CustomException.InvalidUserCsvException {
-		List<NewUser> records = new ArrayList<>();
+	private List<NewUserValidator> fileToNewUserList(MultipartFile file) throws IOException, CustomException.InvalidUserCsvException {
+		List<NewUserValidator> records = new ArrayList<>();
 		Reader reader = new InputStreamReader(file.getInputStream());
 		CSVReader csvReader = new CSVReaderBuilder(reader).withSkipLines(1).build();
 		String[] values = null;
@@ -53,17 +52,17 @@ public class UsersController {
 			if (values.length != 2) {
 				throw new CustomException.InvalidUserCsvException("Invalid CSV. Please check and try again.");
 			}
-			records.add(new NewUser().setName(values[0]).setSalary(values[1]));
+			records.add(new NewUserValidator().setName(values[0]).setSalary(values[1]));
 		}
 		return records;
 	}
 
-	private UserDto saveUser(NewUser newUser) {
+	private UserDto saveUser(NewUserValidator newUser) {
 		UserDto userDto = new UserDto().setName(newUser.getName()).setSalary(Double.parseDouble(newUser.getSalary()));
 		return userService.createOne(userDto);
 	}
 
-	private List<UserDto> saveManyUsers(List<NewUser> newUserList) {
+	private List<UserDto> saveManyUsers(List<NewUserValidator> newUserList) {
 		List<UserDto> userDtoList = newUserList.stream().map(nu -> new UserDto().setName(nu.getName()).setSalary(Double.parseDouble(nu.getSalary()))).collect(Collectors.toList());
 		return userService.createMany(userDtoList);
 	}
